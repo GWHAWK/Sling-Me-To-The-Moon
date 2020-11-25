e404bdd00e9eb6d41293d09248af371dfe15e3eb
GWHAWK <39492210+GWHAWK@users.noreply.github.com>
2020-11-24 19:59:00 -0500

    Basic Game System

---
+++ Sling To The Moon/Assets/RocketScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddForce(transform.forward * 1000);
+            transform.parent = null;
+        }
+    }
+}
